f154d89b714462cae3fbca6f7969140731848b16
masterHAWK99 <masterHAWK99@users.noreply.github.com>
2022-09-30 21:09:52 +0000

    Deploying to gh-pages from @ masterHAWK99/junior-front-end-developer-1664373968@89a00fafd6b36e372f0879a6839780865dd28253 🚀

--- src/component/tasks/TaskItem.jsx
+++
@@ -1,8 +0,0 @@
-export const TaskItem = ({ onClick, title, icon }) => {
-  return (
-    <div className="mt-6 ml-6 flex" onClick={onClick}>
-      <img src={icon} alt="" />
-      <p className="ml-3">{title}</p>
-    </div>
-  );
-};
