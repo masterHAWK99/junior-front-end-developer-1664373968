2c9991630fdfb49f56adad94896943607748515c
masterHAWK99 <maciej.pochron99@gmail.com>
2022-09-30 19:45:08 +0200

    #5 Add profile image

--- src/component/businessContexts/ContextContent.jsx
+++ src/component/businessContexts/ContextContent.jsx
@@ -1,4 +1,5 @@
 import { getLongFormattedDate } from "../../utils/DateFormatingUtils";
+import profile from "../../assets/profile.png";
 
 export const ContextContent = ({ tabContent }) => {
   const { title, content, author, created_at } = tabContent;
@@ -6,13 +7,22 @@ export const ContextContent = ({ tabContent }) => {
   return (
     <div className="p-6">
       <p className="font-semibold text-lg text-dark">{title}</p>
-      <div className="flex align-items-center">
-        <p className="font-medium text-dark">{author}</p>
-        <p className="text-sm ml-1 text-light">
-          {getLongFormattedDate(created_at)}
-        </p>
+      <div className="flex">
+        <img
+          className="rounded-full border border-1 avatar"
+          src={profile}
+          alt="profile"
+        />
+        <div className="ml-2">
+          <div className="flex align-items-center">
+            <p className="font-medium text-dark">{author}</p>
+            <p className="text-sm ml-1 text-light">
+              {getLongFormattedDate(created_at)}
+            </p>
+          </div>
+          <p className="text-sm font-medium text-light">{content}</p>
+        </div>
       </div>
-      <p className="text-sm font-medium text-light">{content}</p>
     </div>
   );
 };
