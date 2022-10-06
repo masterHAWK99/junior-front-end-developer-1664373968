fd4f5b8b3da3b2a279b247ea5d54633ea4843906
masterHAWK99 <maciej.pochron99@gmail.com>
2022-10-06 15:16:17 +0200

    #5 Add medium font weight when task is not active

--- src/component/tasks/TaskItem.jsx
+++ src/component/tasks/TaskItem.jsx
@@ -3,7 +3,9 @@ export const TaskItem = ({ onClick, title, icon, active }) => {
     <div className="mt-6 flex">
       <img src={icon} alt="" />
       <button
-        className={`btn ml-3 text-dark text-md ${active && "font-semibold"}`}
+        className={`btn ml-3 text-dark text-md ${
+          active ? "font-semibold" : "font-medium"
+        }`}
         onClick={onClick}
       >
         {title}
