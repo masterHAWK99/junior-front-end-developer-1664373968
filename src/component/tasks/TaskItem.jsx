fc34607862c64afac54a89d3187e5fa77496c8a0
masterHAWK99 <maciej.pochron99@gmail.com>
2022-10-16 21:18:45 +0200

    #5 Add different text color for blocked task

--- src/component/tasks/TaskItem.jsx
+++ src/component/tasks/TaskItem.jsx
@@ -1,11 +1,29 @@
-export const TaskItem = ({ onClick, title, icon, active }) => {
+import completedIcon from "../../assets/completed.svg";
+import activeIcon from "../../assets/active.svg";
+import blockedIcon from "../../assets/blocked.svg";
+
+export const TaskItem = ({ onClick, title, status }) => {
+  const isActive = status === "active";
+  const isBlocked = status === "blocked";
+
+  const getIconByStatus = () => {
+    if (isActive) {
+      return activeIcon;
+    }
+    if (isBlocked) {
+      return blockedIcon;
+    }
+
+    return completedIcon;
+  };
+
   return (
     <div className="mt-6 flex">
-      <img src={icon} alt="" />
+      <img src={getIconByStatus()} alt="" />
       <button
-        className={`btn btn-empty ml-3 text-dark text-md ${
-          active ? "font-semibold" : "font-medium"
-        }`}
+        className={`btn btn-empty ml-3 ${
+          isBlocked ? "text-gray-light" : "text-dark"
+        } text-md ${isActive ? "font-semibold" : "font-medium"}`}
         onClick={onClick}
       >
         {title}
